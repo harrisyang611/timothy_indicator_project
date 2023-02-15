eb19750bd1eb7b36c4ac03261985f43f369d75ce
harrisyang611 <harrisyang611@outlook.com>
2023-02-15 13:41:06 -0500

    doc & cleanup

--- src/construct_equity.py
+++
@@ -1,7 +0,0 @@
-from joblib import load
-
-
-from joblib import load
-import os
-print(os.listdir())
-model = load( './model_joblib/OEX_ridge_base.joblib')
--- src/model.py
+++ src/model.py
@@ -1,3 +1,6 @@
+## The script we wrote for running everything
+## This script is a bit uncleaned, require further documentations
+
 import pandas as pd
 import numpy as np
 from scipy.stats import spearmanr
@@ -25,11 +28,8 @@ from joblib import dump, load
 
 random.seed(611)
 
-list_file = os.listdir('./data/')
-list_file = [i for i in list_file if '_full' in i]
-print(list_file)
-output_dir = './output_new/' 
 
+## This evaluation list is selected based on our selected tickers
 evaluation_list = [
     {'col': 'base', 'model_name': 'gb', 'ticker_name': 'TLT'},
     {'col': 'RSI_20_ta', 'model_name': 'rf', 'ticker_name': 'TLT'},
@@ -217,95 +217,100 @@ def shap_plot(model, df, model_name,indicator_name, ticker_name, outdir = output
     shap_wrapper(model, X_train, plot_title, plot_location)
 
 
-for my_ticker_file in tqdm(list_file):
-    
-    ticker_name = my_ticker_file.split('_')[0]
-    file_location = './data/' + my_ticker_file
-    print(f'Start modelling for {ticker_name}: ',  datetime.datetime.now())
 
-    df = pd.read_csv(file_location)
-    df['Date'] = pd.to_datetime(df['Date'], format = '%Y%m%d')
+if __name__ == '__main__':
+    data_dir = './data/'
+    list_file = os.listdir(data_dir)
+    list_file = [i for i in list_file if '_full' in i]
+    print(list_file)
+    output_dir = './output_new/' 
 
-    df = df.set_index('Date')
-
-    for n in list(range(1,15)):
-        name = 'ret' + str(n)
-        df[name] = df['Open'].pct_change(periods=n)#for trading with open
-
-    df['retFut1'] = df['Open'].pct_change(1).shift(-1).fillna(0)
-    
-    df['RSI_20_ta'] = ta.RSI(np.array(df['Open']), timeperiod = 20)
-    df['RSI_25_ta'] = ta.RSI(np.array(df['Open']), timeperiod = 25)
-
-    indicator_col = [
-        'RSI_20','RSI_20_ta', 'RSI_25','RSI_25_ta',
-        'DT_RSI_2_20', 'STO_20_1', 'MADIFF_10_100_0', 'MADIFF_10_100',
-        'MACD_10_100_5', 'LINTRND_10', 'PR_INT_0', 'PR_INT_20', 'CMMA_10_252',
-        'ENT_2_10', 'ENT_4_16', 'FTI_LP', 'FTI_BP', 'FTI_BF'
-    ]
-    base_col = ['ret1', 'ret2', 'ret3', 'ret4', 'ret5', 'ret6', 'ret7', 'ret8', 'ret9',
-        'ret10', 'ret11', 'ret12', 'ret13', 'ret14']
-        # , 'ret15', 'ret16', 'ret17',
-        # 'ret18', 'ret19', 'ret20', 'ret21', 'ret22', 'ret23', 'ret24', 'ret25',
-        # 'ret26', 'ret27', 'ret28', 'ret29']
-
-    print(f'Data Prep finished: ',  datetime.datetime.now())
-
-    ## model def
-
-    sharpe_scorer = make_scorer(sharpe, greater_is_better=True)
-    spearmanr_scorer = make_scorer(information_coefficient, greater_is_better=True)
-    scoring = {"rmse": "neg_root_mean_squared_error", 'sharpe': sharpe_scorer, 'spearmanr': spearmanr_scorer}
-    split = TimeSeriesSplit(n_splits=5)
-
-    ## training here
-    numeric_sub_pipeline = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant', fill_value = 0)),
-    ('scaler', StandardScaler())])
-    ridge = Ridge(max_iter=5000) 
-    a_rs = np.logspace(-40, 0, num=100, endpoint = True)
-
-    ridge_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('ridge', ridge)])
-    ridge_param_grid = [{ 'ridge__alpha': a_rs }]
-
-    ridge_df = model_building(df, 'ridge', ridge_pipe, ridge_param_grid)
-    
-    print(f'ridge model finished: ',  datetime.datetime.now())
-
-    rf_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('rf', RandomForestRegressor())])
-    rf_param_grid = [{ 'rf__n_estimators': [250] , 'rf__max_depth':[10,20,30]}]
-
-    rf_df = model_building(df, 'rf', rf_pipe, rf_param_grid)
-
-    print(f'rf model finished: ',  datetime.datetime.now())
-
-    gb_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('gb', GradientBoostingRegressor())])
-    gb_param_grid = [{ 'gb__n_estimators': [250] , 'gb__max_depth':[10,20,30]}]
-
-    db_df = model_building(df, 'gb', gb_pipe, gb_param_grid)
-
-    print(f'gb model finished: ',  datetime.datetime.now())
-
-    myres = pd.concat([ridge_df , rf_df, db_df])
-    for index, row in myres.iterrows():
-        model_tuned = row['model_tuned']
-        out_model_name = output_dir + ticker_name +'_'+ row['model_name'] + '_' + row['col'] + '.joblib'
-        if not os.path.exists(out_model_name):
-            dump(model_tuned, out_model_name) 
+    for my_ticker_file in tqdm(list_file):
         
-        try:
-            for interest_combo in evaluation_list:
-                if( row['model_name'] == interest_combo['model_name'] and  
-                    row['col'] == interest_combo['col'] and
-                    ticker_name == interest_combo['ticker_name'] ):
+        ticker_name = my_ticker_file.split('_')[0]
+        file_location = data_dir + my_ticker_file
+        print(f'Start modelling for {ticker_name}: ',  datetime.datetime.now())
 
-                    shap_plot(model_tuned, df[row['column_names']], row['model_name'], row['col'] , ticker_name)
-                    print(f'shap created for {interest_combo} at: ',  datetime.datetime.now())
-        except:
-            pass
+        df = pd.read_csv(file_location)
+        df['Date'] = pd.to_datetime(df['Date'], format = '%Y%m%d')
 
-    myres = myres.drop(['model_tuned','column_names'], axis=1)
-    outfile = output_dir + ticker_name + '_model_result.csv'
-    myres.to_csv(outfile, index=False)
+        df = df.set_index('Date')
 
-    print(f'model trained, saved at {outfile} at: ',  datetime.datetime.now())
+        for n in list(range(1,15)):
+            name = 'ret' + str(n)
+            df[name] = df['Open'].pct_change(periods=n)#for trading with open
+
+        df['retFut1'] = df['Open'].pct_change(1).shift(-1).fillna(0)
+        
+        indicator_col = [
+            'RSI_20','RSI_20_ta', 'RSI_25','RSI_25_ta',
+            'DT_RSI_2_20', 'STO_20_1', 'MADIFF_10_100_0', 'MADIFF_10_100',
+            'MACD_10_100_5', 'LINTRND_10', 'PR_INT_0', 'PR_INT_20', 'CMMA_10_252',
+            'ENT_2_10', 'ENT_4_16', 'FTI_LP', 'FTI_BP', 'FTI_BF'
+        ]
+        base_col = ['ret1', 'ret2', 'ret3', 'ret4', 'ret5', 'ret6', 'ret7', 'ret8', 'ret9',
+            'ret10', 'ret11', 'ret12', 'ret13', 'ret14']
+            # , 'ret15', 'ret16', 'ret17',
+            # 'ret18', 'ret19', 'ret20', 'ret21', 'ret22', 'ret23', 'ret24', 'ret25',
+            # 'ret26', 'ret27', 'ret28', 'ret29']
+
+        print(f'Data Prep finished: ',  datetime.datetime.now())
+
+        ## model def
+
+        sharpe_scorer = make_scorer(sharpe, greater_is_better=True)
+        spearmanr_scorer = make_scorer(information_coefficient, greater_is_better=True)
+        scoring = {"rmse": "neg_root_mean_squared_error", 'sharpe': sharpe_scorer, 'spearmanr': spearmanr_scorer}
+        split = TimeSeriesSplit(n_splits=5)
+
+        ## training here
+        numeric_sub_pipeline = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='constant', fill_value = 0)),
+        ('scaler', StandardScaler())])
+        ridge = Ridge(max_iter=5000) 
+        a_rs = np.logspace(-40, 0, num=100, endpoint = True)
+
+        ridge_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('ridge', ridge)])
+        ridge_param_grid = [{ 'ridge__alpha': a_rs }]
+
+        ridge_df = model_building(df, 'ridge', ridge_pipe, ridge_param_grid)
+        
+        print(f'ridge model finished: ',  datetime.datetime.now())
+
+        rf_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('rf', RandomForestRegressor())])
+        rf_param_grid = [{ 'rf__n_estimators': [250] , 'rf__max_depth':[10,20,30]}]
+
+        rf_df = model_building(df, 'rf', rf_pipe, rf_param_grid)
+
+        print(f'rf model finished: ',  datetime.datetime.now())
+
+        gb_pipe = Pipeline(steps=[('preprocessor', numeric_sub_pipeline),('gb', GradientBoostingRegressor())])
+        gb_param_grid = [{ 'gb__n_estimators': [250] , 'gb__max_depth':[10,20,30]}]
+
+        db_df = model_building(df, 'gb', gb_pipe, gb_param_grid)
+
+        print(f'gb model finished: ',  datetime.datetime.now())
+
+        myres = pd.concat([ridge_df , rf_df, db_df])
+        for index, row in myres.iterrows():
+            model_tuned = row['model_tuned']
+            out_model_name = output_dir + ticker_name +'_'+ row['model_name'] + '_' + row['col'] + '.joblib'
+            if not os.path.exists(out_model_name):
+                dump(model_tuned, out_model_name) 
+            
+            try:
+                for interest_combo in evaluation_list:
+                    if( row['model_name'] == interest_combo['model_name'] and  
+                        row['col'] == interest_combo['col'] and
+                        ticker_name == interest_combo['ticker_name'] ):
+
+                        shap_plot(model_tuned, df[row['column_names']], row['model_name'], row['col'] , ticker_name)
+                        print(f'shap created for {interest_combo} at: ',  datetime.datetime.now())
+            except:
+                pass
+
+        myres = myres.drop(['model_tuned','column_names'], axis=1)
+        outfile = output_dir + ticker_name + '_model_result.csv'
+        myres.to_csv(outfile, index=False)
+
+        print(f'model trained, saved at {outfile} at: ',  datetime.datetime.now())
